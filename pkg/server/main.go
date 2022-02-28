d392da39a82c38eca618d9daf64f8ece23b74f86
Noam Weiler <noamweiler@Noams-MacBook-Pro.local>
2022-02-28 16:43:21 +0200

    added signal handler to server

--- pkg/server/main.go
+++ pkg/server/main.go
@@ -4,13 +4,22 @@ import (
 	pb "GHZExample/internal/proto_db"
 	context "context"
 	"fmt"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 const (
 	port = 50052
+)
+
+var (
+	s *grpc.Server
 )
 
 type server struct {
@@ -22,30 +31,57 @@ func debug(f string, s string) {
 }
 
 func (s *server) SayHello(ctx context.Context, in *pb.ServerRequest) (*pb.ServerResponse, error) {
-	f := "SayHello"
-	debug(f, in.Name)
-	res := fmt.Sprintf("Hello %s!", in.GetName())
+	f, name := "SayHello", in.GetName()
+	debug(f, name)
+	res := fmt.Sprintf("Hello %s!", name)
 	return &pb.ServerResponse{Res: res}, nil
 }
 
-func main() {
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+func createGRPC() error {
+	ln, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("gRPC server: failed to listen.\nerror:%v", err)
 	}
 
-	s := grpc.NewServer()
+	s = grpc.NewServer()
 	pb.RegisterGreeterServer(s, &server{})
+	log.Printf("server listening at %v", ln.Addr())
+	return s.Serve(ln)
+}
 
-	log.Printf("server listening at %v", lis.Addr())
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+	defer close(interrupt)
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	g.Go(createGRPC)
+
+	select {
+	case in := <-interrupt:
+		fmt.Println(in)
+		break
+	case <-ctx.Done():
+		break
 	}
 
-	defer func() { //cleanup
-		s.Stop()
-		if err := lis.Close(); err != nil {
-			log.Println(err)
-		}
-	}()
+	//put the server up for 10 minutes, then close it gracefully
+	_, shutdownCancel := context.WithTimeout(context.Background(), 10*time.Minute)
+	defer shutdownCancel()
+
+	if s != nil {
+		s.GracefulStop()
+	}
+
+	err := g.Wait()
+	if err != nil {
+		log.Fatalf("server returning an error.\nerror:%v", err)
+	}
+
+	fmt.Println("exit..")
 }
